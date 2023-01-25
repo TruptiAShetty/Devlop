f4aed72fbe1e885ccf07642f1e59b07f515de9a7
Pannekatla Rahamthulla <Rahamthulla.Pannekatla@ltts.com>
2023-01-25 07:10:04 +0530

    updated var in s3 trigger

--- lambda/main.tf
+++ lambda/main.tf
@@ -141,7 +141,7 @@ resource "aws_lambda_permission" "api_gw" {
 
 
 resource "aws_s3_bucket_notification" "my-trigger" {
-    bucket = "${var.aws_s3_bucket_object}"
+    bucket = "${aws_s3_bucket_object.wideui.bucket}"
 
     lambda_function {
         lambda_function_arn = "${aws_lambda_function.lambda_function.arn}"
